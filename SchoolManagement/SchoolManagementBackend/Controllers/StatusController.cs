a655a2c87d73c886bc5295aeb05a2b0a75b1651d
maximilian.baertel <maximilian.baertel@mum.de>
2025-11-20 11:26:59 +0100

    Add Basic Controllers

---
+++ SchoolManagement/SchoolManagementBackend/Controllers/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagementBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> getStatus()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SchoolManagement/SchoolManagementBackend/Controllers/TeachersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagementBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeachersController : ControllerBase
+    {
+        private SchoolManagementContext _dbContext;
+        public TeachersController(SchoolManagementContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
